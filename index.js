2c2089d9a5463ff8d48b6c5a62acf372047044f7
shamkaz04vik-create <shamkaz04vik@gmail.com>
2025-12-03 07:43:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,22 @@ import express from "express";
 import TelegramBot from "node-telegram-bot-api";
 
 const token = process.env.BOT_TOKEN;
-const webAppUrl = process.env.WEBAPP_URL; // URL твоей игры (Render или Netlify)
+const webAppUrl = process.env.WEBAPP_URL;
 
 const bot = new TelegramBot(token, { polling: false });
 const app = express();
 
 app.use(express.json());
 
+// УСТАНАВЛИВАЕМ WEBHOOK ДЛЯ БОТА
+bot.setWebHook(`https://qfox-bot.onrender.com/webhook/${token}`);
+
 // Команда /start
 bot.setMyCommands([
   { command: "/start", description: "Start game" }
 ]);
 
-// Приём webhook от Telegram
+// Приём webhook
 app.post(`/webhook/${token}`, (req, res) => {
   bot.processWebhookUpdate(req.body);
   res.sendStatus(200);
@@ -36,7 +39,7 @@ bot.on("message", (msg) => {
   });
 });
 
-// Старт сервера
-app.listen(3000, () => {
+// Старт сервера (Render сам пробросит порт!)
+app.listen(process.env.PORT || 3000, () => {
   console.log("Bot server running!");
 });
